1c6dc2af1b28da7eb8255bd51c8ed514738607f3
AInoob <ainoob@qq.com>
2014-05-24 01:10:44 -0500

    change the structure to matrix, tired, that's it

--- snake.cpp
+++ snake.cpp
@@ -1,91 +1,102 @@
 #include "stdafx.h"
-
 using namespace std;
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-COORD pos;
 
 boolean ask(string question)
 {
-	char response='x';
+	char response = 'x';
 	cout << question << "enter y or n to choose" << endl;
 	cin >> response;
 	response = tolower(response);
-	while (!(response=='y'||response=='n'))
+	while (!(response == 'y' || response == 'n'))
 	{
 		cout << "invalid input, enter y or n to choose" << endl;
 		cin >> response;
 		response = tolower(response);
 	}
-	if (response=='y')
+	if (response == 'y')
 	{
 		return true;
 	}
 	return false;
 }
 
+int ask(int min, int max, string question)
+{
+	int r;
+	string input;
+	cout << question << endl;
+	char *temp;
+	cin >> input;
+	temp = const_cast<char*>(input.c_str());
+	r = atoi(temp);
+	while (r < min || r>max)
+	{
+		cout << "invalid input, try again" << endl;
+		cin >> input;
+		temp = const_cast<char*>(input.c_str());
+		r = atoi(temp);
+	}
+	return r;
+}
 
+COORD** matrix(int x, int y, COORD value)
+{
+	COORD **m = new COORD*[x];
+	for (int i = 0; i < x; i++)
+	{
+		m[i] = new COORD[y];
+	}
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			m[i][j] = value;
+		}
+	}
+	return m;
+}
 
 void changeSize(int a, int b)
 {
-	SMALL_RECT windowSize = { 0,0,a, b};
+	SMALL_RECT windowSize = { 0, 0, a, b };
 	SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
 }
 
 void gotoxy(int x, int y)
 {
+	COORD pos;
 	pos.X = x;
 	pos.Y = y;
-	SetConsoleCursorPosition(hConsole,pos);
+	SetConsoleCursorPosition(hConsole, pos);
 }
 
-void gotoxy(COORD p)
+void show(int x, int y, string show)
 {
-	gotoxy(pos.X, pos.Y);
+	gotoxy(x * 2, y);
+	cout << show;
 }
-
-
-class Node
-{
-private:
-	COORD pos;
-public: 
-	Node* next;
-	Node* prev;
-	Node(int x,int y)
-	{
-		pos.X = x;
-		pos.Y = y;
-	}
-	COORD getPos()
-	{
-		return pos;
-	}
-};
 
 class Snake
 {
-	int MAX_W = 79, MAX_H = 24;
+	COORD **map;
+	int MAX_W = 39, MAX_H = 24;
 	int times = 0;
 	int point = 0;
+	int length = 0;
 	char direction;
-	boolean dead=false;
-	Node *head;
-	Node *tail;
+	boolean dead = false;
+	COORD head;
+	COORD tail;
 	COORD food;
 public:
 	Snake()
 	{
-		gotoxy(6, 5);
-		cout << "■";
-		gotoxy(6, 4);
-		cout << "■";
-		head = new Node(6, 5);
-		tail = new Node(6, 4);
-		head->next = tail;
-		head->prev = NULL;
-		tail->next = NULL;
-		tail->prev = head;
+		show(3, 5, "■");
+		show(3, 4, "■");
+		head = { 3, 5 };
+		tail = { 3, 4 };
 	}
 	boolean isDead()
 	{
@@ -95,34 +106,28 @@ public:
 	{
 		MAX_W = a;
 		MAX_H = b;
+		COORD temp;
+		temp = { -1, -1 };
+		map = matrix(a+1, b+1,temp);
+		map[head.X][head.Y] = tail;
+		length = 2;
 	}
-	boolean checkFood(int x, int y)
+	void end()
 	{
-		Node *current = head;
-		while (current != NULL)
-		{
-			if ((x == current->getPos().X) && (y == current->getPos().Y))
-			{
-				return true;
-			}
-			current = current->next;
-		}
-		return false;
+		delete map;
 	}
 	boolean paintItSelf(int x, int y)
 	{
-		Node *current = head;
-		while (current->next != NULL)
+		if (map[x][y].X == -1)
 		{
-			if ((x == current->getPos().X) && (y == current->getPos().Y))
-			{
-				return true;
-			}
-			current = current->next;
+			return false;
 		}
-		return false;
+		else
+		{
+			return true;
+		}
 	}
-	boolean eatItSelf(int x,int y)
+	boolean eatItSelf(int x, int y)
 	{
 		if (paintItSelf(x, y))
 		{
@@ -134,22 +139,47 @@ public:
 			return false;
 		}
 	}
+	COORD getTail()
+	{
+		COORD current=head;
+		for (int i = 0; i < length-1; i++)
+		{
+			current = map[current.X][current.Y];
+		}
+		return current;
+	}
+	COORD getTailPrev()
+	{
+		COORD current = head;
+		for (int i = 0; i < length - 2; i++)
+		{
+			current = map[current.X][current.Y];
+		}
+		return current;
+	}
 	void makeFood()
 	{
 		times++;
 		point = point + times;
-		int x, y;
-		while (true)
+		int r = rand() % (MAX_W*MAX_H-length);
+		for (int i = 0; i < MAX_W; i++)
 		{
-			x = rand() % (MAX_W/2) * 2;
-			y = rand() % MAX_H;
-			if (!checkFood(x, y))
-				break;
+			for (int j = 0; j < MAX_H; j++)
+			{
+				if (map[i][j].X == -1)
+				{
+					r--;
+					if (r == -1)
+					{
+						food.X = i;
+						food.Y = j;
+						show(food.X, food.Y, "★");
+						return;
+					}
+				}
+			}
 		}
-		food.X = x;
-		food.Y = y;
-		gotoxy(x, y);
-		cout << "★";
+		return;
 	}
 	boolean hitWall(int x, int y)
 	{
@@ -160,36 +190,35 @@ public:
 		}
 		return false;
 	}
-	void move(int x,int y)
+	void move(int x, int y)
 	{
-		gotoxy(0, 0);
-		cout << "Score: " << point;
-		Node *newHead = new Node(x, y);
-		head->prev = newHead;
-		newHead->next = head;
+		show(0, 0, "Score: ");
+		cout <<point;
+		COORD newHead = {x,y};
+		map[newHead.X][newHead.Y] = head;
 		head = newHead;
-		gotoxy(head->getPos().X, head->getPos().Y);
-		cout << "■";
-		if ((food.X == x)&&(food.Y == y))
+		show(head.X, head.Y, "■");
+		if ((food.X == x) && (food.Y == y))
 		{
 			makeFood();
+			length++;
 		}
 		else
 		{
-			gotoxy(tail->getPos().X, tail->getPos().Y);
-			if (!paintItSelf(tail->getPos().X, tail->getPos().Y))
+			COORD temp = getTail();
+			if (!paintItSelf(tail.X, tail.Y))
 			{
-				cout << "  ";
+				show(tail.X, tail.Y, "  ");
+				map[temp.X][temp.Y] = { -1, -1 };
 			}
-			tail = tail->prev;
-			tail->next = NULL;
+			tail = temp;
 		}
 	}
 	void headTo(char d)
 	{
 		direction = d;
 		COORD headPos;
-		headPos = head->getPos();
+		headPos = head;
 		switch (direction)
 		{
 		case 'w':
@@ -199,33 +228,45 @@ public:
 			headPos.Y = headPos.Y + 1;
 			break;
 		case 'a':
-			headPos.X = headPos.X - 2;
+			headPos.X = headPos.X - 1;
 			break;
 		case 'd':
-			headPos.X = headPos.X + 2;
+			headPos.X = headPos.X + 1;
 			break;
 		}
-		if ((!eatItSelf(headPos.X, headPos.Y)) && (!hitWall(headPos.X, headPos.Y)))
+		if ((!hitWall(headPos.X, headPos.Y) && (!eatItSelf(headPos.X, headPos.Y))))
 		{
-			move(headPos.X,headPos.Y);
+			move(headPos.X, headPos.Y);
 		}
 	}
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int w, h;
+	SetConsoleTitle(L"SNAKE");
+	int w, h, temp = 0;
 	srand((int)time(NULL));
-	double speed=100;
+	double speed = 100;
 	gotoxy(0, 0);
 	if (ask("Do you want to customize settings?"))
 	{
-		cout << "input width(max 39) and height(max 24)" << endl;
-		cin >> w >> h;
-		w = w * 2 + 1;
-		changeSize(w, h);
-		cout << "j and k can change the speed of snake" << endl;
+		cout << "input width(max 39, min 10) and height(max 24, min 10)" << endl;
+		w=ask(10, 39, "input the width");
+		h=ask(10, 24, "input the height");
+		changeSize(w*2+1, h);
+	}
+	else
+	{
+		w = 39;
+		h = 24;
+	}
+	cout << "j and k can change the speed of snake" << endl << "enter space to pause, then press any move key to resume" << endl;
+	cout << "enter q to exit" << endl;
+	while (temp < 3)
+	{
 		Sleep(1000);
+		temp++;
+		cout << 4 - temp << endl;
 	}
 	system("cls");
 	while (true)
@@ -241,6 +282,11 @@ int _tmain(int argc, _TCHAR* argv[])
 			cout << "Speed: " << speed;
 			if (_kbhit())
 				newDirection = _getch();
+			while (newDirection == ' ')
+			{
+				if (_kbhit())
+					newDirection = _getch();
+			}
 			if (newDirection == 224 || newDirection == 0 || newDirection == -32)
 			{
 				newDirection = _getch();
@@ -260,19 +306,24 @@ int _tmain(int argc, _TCHAR* argv[])
 					break;
 				}
 			}
+
 			newDirection = (char)towlower(newDirection);
 			if (newDirection == 'j')
 			{
 				speed = 1.1*speed;
 				newDirection = 'x';
 			}
-			else if (newDirection=='k')
+			else if (newDirection == 'k')
 			{
-				if (speed>=3)
-					speed = speed/1.1;
+				if (speed >= 3)
+					speed = speed / 1.1;
 				newDirection = 'x';
 			}
-			if (newDirection == 'w' || newDirection == 'a' || newDirection == 's' || newDirection == 'd')
+			else if (newDirection == 'q')
+			{
+				return 0;
+			}
+			else if (newDirection == 'w' || newDirection == 'a' || newDirection == 's' || newDirection == 'd')
 			{
 				if ((direction == 'w'&&newDirection != 's') || (direction == 's'&&newDirection != 'w') || (direction == 'a'&&newDirection != 'd') || (direction == 'd'&&newDirection != 'a'))
 				{
@@ -283,6 +334,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			Sleep((int)speed);
 		}
 		gotoxy(0, 6);
+		s.end();
 		cout << "Game over! Press enter to restart";
 		while (newDirection != 13)
 		{
